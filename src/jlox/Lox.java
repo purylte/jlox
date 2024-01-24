6cf8a31228e2d4547dc530ddf1ce9831329e6e73
purylte <purylte@gmail.com>
2024-01-24 15:27:13 +0000

    call intepreter in main file

--- src/jlox/Lox.java
+++ src/jlox/Lox.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
     static boolean hadRuntimeError = false;
 
@@ -59,7 +60,7 @@ public class Lox {
         if (hadError)
             return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
